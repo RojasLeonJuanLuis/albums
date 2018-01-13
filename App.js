b6229f2d786c2a53e3e30a9b9a456be00688fafc
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-13 14:20:47 -0600

    consuming file exports

--- App.js
+++ App.js
@@ -5,12 +5,13 @@
  */
 
 import React, { Component } from 'react';
+import Header from './src/components/header'
 
 
 export default class App extends Component<{}> {
   render() {
     return (
-
+      <Header />
     )
   }
 }
