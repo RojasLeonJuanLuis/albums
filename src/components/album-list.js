4b291b1e2efe35cb6df6f7a224d44bdcf98da644
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-01-13 22:50:22 -0600

    lifecycle methods

--- src/components/album-list.js
+++ src/components/album-list.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class AlbumList extends Component {
+
+  componentWillMount() {
+    console.log('component componentWillMount');
+  }
+
   render() {
     return (
       <View>
